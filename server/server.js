fe8fdf88e05f0fd5b2649df33e8b41bc062b5634
kodykam <bigachiever@icloud.com>
2026-04-26 08:17:07 -0400

    Fix server.js PORT 5000

--- server/server.js
+++ server/server.js
@@ -4,4 +4,8 @@ import { connectDB } from "./config/db.js"
 
 connectDB()
 
-app.listen(5000, () => console.log("Server running"))
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
